9f72273e69ef4b20b9c40da94179e0866c89eeef
marjia-anee <marjiazaman@gmail.com>
2020-12-20 23:24:37 +0600

    first

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { ObjectId } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config()
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tantc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get('/', (req, res) =>{
+  res.send('hello from db')
+})
+
+const client = new MongoClient(uri, {useUnifiedTopology: true , useNewUrlParser: true });
+client.connect(err => {
+  const moviesCollection = client.db("myCineplex").collection("movies");
+  console.log('database connected');
+
+  const registersCollection = client.db("myCineplex").collection("registers");
+
+  app.post('/addMovies', (req, res) => {
+    const movies = req.body;
+    console.log(movies)
+    moviesCollection.insertOne(movies)
+    .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount)
+        console.log(result);
+    })
+
+  })
+
+  app.get('/movies', (req, res) => {
+    moviesCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.get('/movie/:_id', (req, res) => {
+    moviesCollection.find({_id: ObjectId(req.params._id) })
+    .toArray((err, documents) => {
+      res.send(documents[0]);
+    })
+  })
+
+
+  app.post('/addReg', (req, res) => {
+    const registers = req.body;
+    registersCollection.insertOne(registers)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+        console.log(result);
+        console.log(registers);
+    })
+  })
+
+  app.get('/regDetails', (req, res) => {
+    registersCollection.find({email: req.query.email})
+    .toArray((err, documents) => {
+        res.send(documents);
+    })
+  })
+
+
+  app.delete('/regDetailsDelete/:_id', (req, res) => {
+    registersCollection.deleteOne({_id: req.params._id})
+    .then( result => {
+      console.log(result);
+    })
+  })
+
+
+});
+
+
+app.listen(process.env.PORT || port);
